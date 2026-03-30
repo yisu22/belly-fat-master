675a1dd06774b02570d66ca5301b45c080684db8
yisu22 <yisu22@gmail.com>
2026-03-30 16:55:30 +0900

    Remove missing pwa import

--- main.tsx
+++ main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
-import { registerServiceWorker } from './utils/pwa';
+//import { registerServiceWorker } from './utils/pwa';
 
 registerServiceWorker();
 
